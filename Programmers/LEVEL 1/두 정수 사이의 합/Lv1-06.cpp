bb9c6ebd21cc9fabe9dd3d033e12f1dd21966852
JeongNi <59482764+JeongEunBae@users.noreply.github.com>
2020-02-22 20:44:05 +0900

    Create Lv1-06.cpp

---
+++ Programmers/LEVEL 1/두 정수 사이의 합/Lv1-06.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+long long solution(int a, int b) {
+    long long answer = 0;
+    if (a > b) { // 대소관계 구별
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+    long long len = b - a + 1; // 길이
+    if (len % 2==0) answer += ((long long)a + (long long)b) * (len/2); // 길이가 짝수일 때
+    else answer += (((long long)a + (long long)b) * ((len-1)/2)) + (((long long)a + (long long)b) / 2); // 길이가 홀수 일때
+    return answer;
+}
