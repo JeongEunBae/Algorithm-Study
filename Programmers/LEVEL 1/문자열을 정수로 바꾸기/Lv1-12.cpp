429ef41a1b97250d41252cb85d1b3b245d54700e
JeongNi <59482764+JeongEunBae@users.noreply.github.com>
2020-02-28 21:31:18 +0900

    Create Lv1-12.cpp

---
+++ Programmers/LEVEL 1/문자열을 정수로 바꾸기/Lv1-12.cpp
@@ -0,0 +1,10 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+int solution(string s) {
+    int answer = 0;
+    answer = stoi(s);//string에서 int로 변환
+    return answer;
+}
