27e26d6b9398aa4599aec0e5452f143170a7d561
JeongNi <59482764+JeongEunBae@users.noreply.github.com>
2020-03-17 00:24:23 +0900

    Create Lv1-23.cpp

---
+++ Programmers/LEVEL 1/자연수 뒤집어 배열로 만들기/Lv1-23.cpp
@@ -0,0 +1,13 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+vector<int> solution(long long n) {
+    vector<int> answer;
+    while (n > 0) {
+        answer.push_back(n % 10); // 10으로 나눈 나머지 벡터에 추가
+        n /= 10;
+    }
+    return answer;
+}
